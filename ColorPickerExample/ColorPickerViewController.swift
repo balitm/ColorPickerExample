26ca49f1b239540e5421e74c413d215485b0faaa
Balázs Kilvády <bkilvady@gmail.com>
2015-09-30 13:19:26 +0200

    Improve handling of the color palette.

--- ColorPickerExample/ColorPickerViewController.swift
+++ ColorPickerExample/ColorPickerViewController.swift
@@ -31,27 +31,43 @@ THE SOFTWARE.
 import UIKit
 
 class ColorPickerViewController: UIViewController {
+
+	private static var kRows = 10
+	private static var kColumns = 16
+	private static let _colorPalette: [UIColor] = {
+		// Get colorPalette array from plist file
+		let path = NSBundle.mainBundle().pathForResource("colorPalette", ofType: "plist")
+		guard let pListArray = NSArray(contentsOfFile: path!) else {
+			return [UIColor]()
+		}
+
+		var colors = [UIColor]()
+		for hexString in pListArray {
+			let color = ColorPickerViewController.hexStringToUIColor(hexString as! String)
+			colors.append(color)
+		}
+		return colors
+	}()
 	
-	// Global variables
-	var tag: Int = 0
 	var color: UIColor = UIColor.grayColor()
 	var delegate: ViewController? = nil
-	
-	// This function converts from HTML colors (hex strings of the form '#ffffff') to UIColors
-	func hexStringToUIColor(hex: String) -> UIColor {
+
+
+	// This function converts from HTML colors (hex strings of the form '#ffffff') to UIColors.
+	private static func hexStringToUIColor(hex: String) -> UIColor {
 		var cString: String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
-		
+
 		if (cString.hasPrefix("#")) {
 			cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
 		}
-		
+
 		if (cString.characters.count != 6) {
 			return UIColor.grayColor()
 		}
-		
+
 		var rgbValue:UInt32 = 0
 		NSScanner(string: cString).scanHexInt(&rgbValue)
-		
+
 		return UIColor(
 			red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
 			green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
@@ -64,19 +80,19 @@ class ColorPickerViewController: UIViewController {
 extension ColorPickerViewController: UICollectionViewDataSource {
 	// Returns the number of rows in a section of the collection view.
 	internal func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 10
+		return ColorPickerViewController.kRows
 	}
 
 	// Returns the number of sections (columns) in the collection view.
 	internal func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-		return 16
+		return ColorPickerViewController.kColumns
 	}
 
 	// Inilitializes the collection view cells
 	internal func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
 		cell.backgroundColor = UIColor.clearColor()
-		cell.tag = tag++
+		cell.tag = indexPath.section * ColorPickerViewController.kRows + indexPath.item
 
 		return cell
 	}
@@ -85,20 +101,9 @@ extension ColorPickerViewController: UICollectionViewDataSource {
 extension ColorPickerViewController: UICollectionViewDelegateFlowLayout {
 	// Handles when a collection view cell has been selected.
 	internal func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-		var colorPalette: [String]
-
-		// Get colorPalette array from plist file
-		let path = NSBundle.mainBundle().pathForResource("colorPalette", ofType: "plist")
-		let pListArray = NSArray(contentsOfFile: path!)
-
-		if let colorPalettePlistFile = pListArray {
-			colorPalette = colorPalettePlistFile as! [String]
-
-			let cell: UICollectionViewCell  = collectionView.cellForItemAtIndexPath(indexPath)! as UICollectionViewCell
-			let hexString = colorPalette[cell.tag]
-			color = hexStringToUIColor(hexString)
-			self.view.backgroundColor = color
-			delegate?.setButtonColor(color)
-		}
+		let cell = collectionView.cellForItemAtIndexPath(indexPath)! as UICollectionViewCell
+		color = ColorPickerViewController._colorPalette[cell.tag]
+		self.view.backgroundColor = color
+		delegate?.setButtonColor(color)
 	}
 }
